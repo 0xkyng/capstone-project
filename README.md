406d381800fc2ecb11184cf35f07ef4fe80202c1
Isaac Wanger <teryima127@gmail.com>
2023-07-07 12:16:10 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# CUSD NFT MINTER DAPP
+
+#### NFT minter decentralised application
+
+- [Demo App](https://bafybeiaisb5mh4v72fccro36dsucaczu6uxhwhkjglzz3fm5pene4q7iwa.ipfs.sphn.link/)
+
+
+
+## About the dapp
+Nft Minter is a decentralized minting app that allows users to mint their unique Nfts with built in name and description using the Nft Url generated from [pinata(ipfs)](https://www.pinata.cloud/).
+Its Built on the [Celo(alfajores testnet)](https://docs.celo.org/network) network.
+
+## Getting started for Developers
+
+- Clone the repository by running the command:
+```bash
+git clone https://github.com/pelzfx/Cusd-Minter-Dapp
+```
+_Please ensure you have Git installed before running the above command and subsequent commands_
+
+- Then install dependencies by running the command 
+
+```bash
+- npm install
+- npm install web3 @celo/contractkit 
+```
+
+- Compile the contracts using the command
+````bash
+npx hardhat compile
+````
+
+- To deploy, run the command:
+
+```bash
+npx hardhat run scripts/deploy.js --network alfajores
+```
+- To launch on localHost run the command:
+
+```bash
+npm start
+```
+
+> **Note** Please ensure to create a `.env` file in your project directory and add your secret keys before running the deployment command. Check out the `.env-sample` file to see how to structure your `.env` file.
+
+
+To learn more about Hardhat, check out the [Hardhat documentation](https://hardhat.org/hardhat-runner/docs/getting-started)
+To learn more about Celo, check out the [Celo documentation](https://docs.celo.org/)
---
+++ contracts/ColabToken.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+/// @title Colab Token Contract
+/// @notice NFT contract for celo 
+contract ColabToken is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("ColabToken", "COT") {}
+
+    /// @notice Mints a new token and assign the function caller to be the owner
+    /// @param uri Token metadata URI
+    function safeMint(string calldata uri) public {
+        require(bytes(uri).length > 0, "Empty uri");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    /// @notice Keeps track of total number of tokens minted 
+    /// @return Total number of tokens minted 
+    function tokenCounter() public view returns (uint256) {
+        return  _tokenIdCounter.current();
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
